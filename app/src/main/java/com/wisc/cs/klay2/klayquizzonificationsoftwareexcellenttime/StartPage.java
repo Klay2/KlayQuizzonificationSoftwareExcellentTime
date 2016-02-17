e693e32a50c7f421bd9283a9a18141b699a79c76
klay2 <klaykrueger@wisc.edu>
2016-02-17 13:50:08 -0600

    First commit

---
+++ app/src/main/java/com/wisc/cs/klay2/klayquizzonificationsoftwareexcellenttime/StartPage.java
@@ -0,0 +1,27 @@
+package com.wisc.cs.klay2.klayquizzonificationsoftwareexcellenttime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class StartPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_page);
+    }
+
+
+    public void startQuizz(View view){
+        int correctAns;
+        //pull up fragments record correct answers in this method
+
+    }
+
+
+
+
+}
+
+
