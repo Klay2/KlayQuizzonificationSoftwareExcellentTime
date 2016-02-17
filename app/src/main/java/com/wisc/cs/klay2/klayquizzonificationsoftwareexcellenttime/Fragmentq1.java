15dcfa812c52255f207ea26b0340abb56c7b24f9
klay2 <klaykrueger@wisc.edu>
2016-02-17 16:32:43 -0600

    started to create intent to pass to first fragment

---
+++ app/src/main/java/com/wisc/cs/klay2/klayquizzonificationsoftwareexcellenttime/Fragmentq1.java
@@ -0,0 +1,3 @@
+package com.wisc.cs.klay2.klayquizzonificationsoftwareexcellenttime;
+public class Fragmentq1 {
+}
--- app/src/main/java/com/wisc/cs/klay2/klayquizzonificationsoftwareexcellenttime/StartPage.java
+++ app/src/main/java/com/wisc/cs/klay2/klayquizzonificationsoftwareexcellenttime/StartPage.java
@@ -14,7 +14,12 @@ public class StartPage extends AppCompatActivity {
 
 
     public void startQuizz(View view){
-        int correctAns;
+        int correctAns = 2;//which button is the correct choice
+        String choice1 = "A Natural Death";
+        String choice2 = "Mechanical Hand";
+        String Choice3 = "Desperate Living";
+        int totalCorrect = 0;//total correct answers
+        String question = "Which Horse:The Band album contains the song: 'Taken by vultures'?";
         //pull up fragments record correct answers in this method
 
     }
