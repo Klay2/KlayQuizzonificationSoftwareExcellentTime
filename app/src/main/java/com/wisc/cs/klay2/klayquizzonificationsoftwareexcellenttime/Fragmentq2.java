dbbb74b06acdb230e7fb0076565c249194f058fd
klay2 <klaykrueger@wisc.edu>
2016-02-19 14:29:32 -0600

    added fragmentq2 layout and next button

--- app/src/main/java/com/wisc/cs/klay2/klayquizzonificationsoftwareexcellenttime/Fragmentq2.java
+++ app/src/main/java/com/wisc/cs/klay2/klayquizzonificationsoftwareexcellenttime/Fragmentq2.java
@@ -1,7 +1,15 @@
 package com.wisc.cs.klay2.klayquizzonificationsoftwareexcellenttime;
 
+import android.app.Fragment;
+import android.widget.EditText;
+import android.widget.TextView;
+
 /**
  * Created by klay2 on 2/19/16.
  */
-public class Fragmentq2 {
+public class Fragmentq2 extends Fragment {
+    private TextView header;
+    private TextView questionText;
+    private EditText input;
+
 }
